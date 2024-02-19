76232b1ef38ddd81be354a852818959a44aa7c58
otakuict <otaku.ict@gmail.com>
2024-02-20 01:28:47 +0700

    add slide overlay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import Overlay from "@/components/slidOverleys/overlay";
+
 export default function Home() {
   return (
-    <div className="bg-red-500">
-      <h1>Hello world</h1>
+    <div className="overlay-slide">
+      <div className="bg-red-500">
+        <h1>Hello world</h1>
+      </div>
+
+      <div className="content">
+        <h2>Your Slide Content Here</h2>
+      </div>
     </div>
   );
 }
