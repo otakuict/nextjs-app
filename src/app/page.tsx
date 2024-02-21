1a60c6490b2d489fa7d28cd5a6f608c63ca47657
otakuict <otaku.ict@gmail.com>
2024-02-22 01:32:33 +0700

    add circuit svg concept

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,30 @@ import Overlay from "@/components/slidOverleys/overlay";
 
 export default function Home() {
   return (
-    <div className="overlay-slide">
-      <div className="bg-red-500">
-        <h1>Hello world</h1>
+    <div className="container">
+      <div className="left-overlay">
+        <div className="faiz-icon">( Faize</div>
       </div>
-
-      <div className="content">
-        <h2 className="text-black">Standing by Complete</h2>
-
-        <button
-          type="button"
-          className="enter-btn text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-        >
-          Default
-        </button>
+      <div className="somthing">
+        <svg width="600" height="220" xmlns="http://www.w3.org/2000/svg">
+          <polygon points="50,10 0,190 100,190" fill="lime" stroke="red" />
+          <rect
+            width="150"
+            height="100"
+            x="120"
+            y="50"
+            fill="yellow"
+            stroke="red"
+          />
+          <circle r="45" cx="350" cy="100" fill="pink" stroke="blue" />
+          <text x="420" y="100" fill="red" stroke="blue">
+            I love SVG!
+          </text>
+          Sorry, your browser does not support inline SVG.
+        </svg>
+      </div>
+      <div className="right-overlay">
+        <div className="faiz-icon-right">)</div>
       </div>
     </div>
   );
