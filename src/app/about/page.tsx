5776e03f32315764ff6b83c0093c093d51571ebf
otakuict <otaku.ict@gmail.com>
2024-02-21 15:57:31 +0700

    POC overlay

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,15 @@
+import "./page.css";
+
 export default function Home() {
-  return <h1>about</h1>;
+  return (
+    <div className="container">
+      <div className="left-overlay">
+        <div className="faiz-icon">( Faize</div>
+      </div>
+      <div className="somthing"></div>
+      <div className="right-overlay">
+        <div className="faiz-icon-right">)</div>
+      </div>
+    </div>
+  );
 }
